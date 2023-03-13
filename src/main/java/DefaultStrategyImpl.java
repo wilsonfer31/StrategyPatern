05c0b8e4f40466558be060c1087e8c43418cba31
WilsonFernandes <wilsonfernandes31470@gmail.com>
2023-03-13 16:12:18 +0100

    first commit

---
+++ src/main/java/DefaultStrategyImpl.java
@@ -0,0 +1,8 @@
+public class DefaultStrategyImpl implements Strategy {
+    @Override
+    public void operationStrategy() {
+        System.out.println("===================================");
+        System.out.println("=========== Default Strategy ============");
+        System.out.println(":::::::::::::::::::::::::::::::::::");
+    }
+}
---
+++ src/main/java/StrategyImpl1.java
@@ -0,0 +1,7 @@
+
+public class StrategyImpl1  implements Strategy {
+    @Override
+    public void operationStrategy() {
+        System.out.println("++++++++++ Strategy 1 ++++++++++");
+    }
+}
